ad18772af52456683f1614760cc51a1999224858
Lpxmari <142037709+Lpxmari@users.noreply.github.com>
2025-05-27 12:33:28 -0500

    Datos cargados

--- BrainCircle/src/main/java/co/edu/uniquindio/braincircle/HelloApplication.java
+++ BrainCircle/src/main/java/co/edu/uniquindio/braincircle/HelloApplication.java
@@ -2,8 +2,10 @@ package co.edu.uniquindio.braincircle;
 
 import co.edu.uniquindio.braincircle.Arbol.ArbolBinarioContenido;
 import co.edu.uniquindio.braincircle.Enums.Materia;
+import co.edu.uniquindio.braincircle.Enums.NivelPrioridad;
 import co.edu.uniquindio.braincircle.controlers.ControladorPrincipal;
 import co.edu.uniquindio.braincircle.models.Contenido;
+import co.edu.uniquindio.braincircle.models.Estudiante;
 import co.edu.uniquindio.braincircle.models.Usuario;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -39,6 +41,31 @@ public class HelloApplication extends Application {
         controlador.crearGrupoEstudio("g2", "Estructura", "Grupo de estudio de estructuras básicas de programación", Materia.ESTRUCTURA);
         controlador.crearGrupoEstudio("g3", "Biología I", "Grupo de estudio de teoria fundamental bilogica", Materia.BIOLOGIA);
 
+        controlador.crearSolicitud(anye, NivelPrioridad.ALTA, "Nececito ayuda con una asesoria para ingles", "Hola, me llamo anye, estoy estudiando ingles y necesito ayuda para un taller. Agradesco inmediates");
+        controlador.crearSolicitud(santiago, NivelPrioridad.BAJA, "Nececito un grupo de matematicas", "Hola, me llamo santiago, es qeu no encuentro ningun grupo que contenga información de la asgnatura matemáticas");
+        controlador.crearSolicitud(carlos, NivelPrioridad.MEDIA, "Nececito ayuda con una asesoria para estructura", "Hola, me llamo carlos, estoy estudiando estructura en la universida y necesito ayuda para un parcial. Gracias por la colaboración");
+        controlador.crearSolicitud(mafe, NivelPrioridad.BAJA, "No encuentro un grupo de sociales", "Hola, me llamo mafe, es qeu no encuentro ningun grupo que contenga información de la asgnatura de cieencias sociales");
+
+        controlador.agregarMiembro("g1", (Estudiante) luisa);
+        controlador.agregarMiembro("g1", (Estudiante) carlos);
+        controlador.agregarMiembro("g1", (Estudiante) anye);
+        controlador.agregarMiembro("g1", (Estudiante) mafe);
+        controlador.agregarMiembro("g2", (Estudiante) anye);
+        controlador.agregarMiembro("g2", (Estudiante) santiago);
+        controlador.agregarMiembro("g2", (Estudiante) carlos);
+
+        controlador.enviarMensajeAGrupo("g1", luisa.getId(), "Hola tienen el taller pasado");
+        controlador.enviarMensajeAGrupo("g1", mafe.getId(), "Si ya lo mando");
+        controlador.enviarMensajeAGrupo("g1", mafe.getId(), "doc.docx");
+        controlador.enviarMensajeAGrupo("g1", carlos.getId(), "Hola mando preguntas del listening");
+        controlador.enviarMensajeAGrupo("g1", anye.getId(), "Hay un video en youtube que nos puede ayudar");
+        controlador.enviarMensajeAGrupo("g2", anye.getId(), "Hola, cómo van con el repositorio");
+        controlador.enviarMensajeAGrupo("g2", carlos.getId(), "Bien ya subi los cambios");
+
+
+        controlador.enviarMensaje(anye, luisa, "Hola me podrias mandar el taller que subieron que se me borro");
+        controlador.enviarMensaje(luisa, anye, "Hola, claro ya te lo mando");
+        controlador.enviarMensaje(anye, luisa, "¿Cómo te fue en el parcial?");
 
         controlador.agregarContenido(new Contenido<>("1", "Estructuras", Materia.ESTRUCTURA.toString(), "ARCHIVO", anye.getId(), "/C:/Users/anyie/OneDrive/Documentos/2025-000390.pdf"));
         controlador.agregarContenido(new Contenido<>("2", "Álgebra", Materia.MATEMATICAS.toString(), "VIDEO", anye.getId(), "video.mp4"));
